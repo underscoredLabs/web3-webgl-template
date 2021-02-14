c9fd7d7732f64069288ca88c2613e12487cb3726
leon-do <leondo123@gmail.com>
2021-02-13 21:48:35 -0500

    on change

--- Web3Template/TemplateData/web3Connect.js
+++ Web3Template/TemplateData/web3Connect.js
@@ -2,6 +2,9 @@ if (window.ethereum) {
   web3 = new Web3(window.ethereum);
   // connect popup
   ethereum.enable();
-} else {
-  alert("Non-Ethereum browser detected. Please connect to a wallet");
+
+  window.ethereum.on("accountsChanged", function () {
+    location.reload();
+  });
+
 }
